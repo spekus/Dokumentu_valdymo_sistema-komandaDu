7d26add81c43902dac8c175bd758f7787189eeea
AnnaP <ana.sachova@gmail.com>
2019-02-01 08:18:48 +0200

    REACT: work in progress

--- react/src/Components/Documents/DocumentsListSimple.js
+++ react/src/Components/Documents/DocumentsListSimple.js
@@ -4,23 +4,21 @@ const DocumentsListSimple = (props) => {
     return (
         <React.Fragment>
             <table class="table">
-                <thead style={{backgroundColor:'#EEEEEE'}}>
-                    <tr>
-                        <th>Dokumento tipas</th>
-                        <th>Autorius</th>
-                        <th className='lastColumn'>Sukurimo data</th>
-                    </tr>
+                <thead style={{backgroundColor: '#EEEEEE'}}>
+                <tr>
+                    <th>Dokumento tipas</th>
+                    <th>Autorius</th>
+                    <th className='lastColumn'>Sukurimo data</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {props.list.map(item => (
-                        <tr key={item.id}>
-                            <td>
-                                {item.doctype}
-                            </td>
-                            <td>{item.author}</td>
-                            <td className='lastColumn'>{item.date}</td>
-                        </tr>
-                    ))}
+                {props.list.map(item => (
+                    <tr key={item.id}>
+                    <td>{item.doctype}</td>
+                    <td>{item.author}</td>
+                    <td className='lastColumn'>{item.date}</td>
+                    </tr>
+                ))}
                 </tbody>
             </table>
         </React.Fragment>
