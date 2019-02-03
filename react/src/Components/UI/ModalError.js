b1a703f19d904a7ad1a727704fe73b50a7271dda
AnnaP <ana.sachova@gmail.com>
2019-02-03 02:29:29 +0200

    REACT: Modal Error aptvarkymas

--- react/src/Components/UI/ModalError.js
+++ react/src/Components/UI/ModalError.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 const ModalError = (props) => {
     return (
-        <div className="modal fade" id="modalError" tabIndex="-1" role="dialog"
-             aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div className="modal fade" id="modalError" role="dialog">
             <div className="modal-dialog modal-dialog-centered" role="document">
                 <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <div className="modal-header alert-danger">
+                        <h5 className="modal-title">Klaida!</h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -16,17 +15,12 @@ const ModalError = (props) => {
                         {props.errorText}
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Uždaryti</button>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 };
-
-const raiseError = (error) => {
-
-}
 
 export default ModalError;
