0d172745865d1b8367a11fab072abdde2036e8af
AnnaP <ana.sachova@gmail.com>
2019-02-08 14:30:43 +0200

    SPRING SECURITY: pom.xml pakeitimas, Mvc ismetimas

--- spring/src/main/java/it/akademija/App.java
+++ spring/src/main/java/it/akademija/App.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
