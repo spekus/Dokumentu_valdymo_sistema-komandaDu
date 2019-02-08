9905861feed458188e1a0eec7f73b80f3eb1f8d6
Ivan <ivanas.kirsanov@gmail.com>
2019-02-08 10:48:28 +0200

    testai

--- spring/src/main/java/it/akademija/users/service/UserService.java
+++ spring/src/main/java/it/akademija/users/service/UserService.java
@@ -14,6 +14,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -62,12 +66,47 @@ public class UserService {
     @Transactional
     public void addNewUser(String userIdentifier, String firstname, String lastname, String username, String password) {
         UserEntity userEntity = new UserEntity(userIdentifier, firstname, lastname, username, password);
+
+        get_SHA_1_SecurePassword(userEntity.getPassword());
         UserEntity userEntityFromDataBase1 = userRepository.findUserByUserIdentifier(userIdentifier);
         UserEntity userEntityFromDataBase2 = userRepository.findUserByUsername(username);
         if (userEntityFromDataBase1 == null && userEntityFromDataBase2 == null) {
             userRepository.save(userEntity);
         }
 
+    }
+
+    private static String get_SHA_1_SecurePassword(String passwordToHash)
+    {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            //md.update(salt);
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        //Create array for salt
+        byte[] salt = new byte[16];
+        //Get a random salt
+        sr.nextBytes(salt);
+        //return salt
+        return salt;
     }
 
     @Transactional
